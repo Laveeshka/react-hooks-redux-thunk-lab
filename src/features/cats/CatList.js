51a148c64a1d4100c6737dc971f8c50c7c1c4127
LaveeshkaMahadea <laveeshka94@gmail.com>
2023-01-16 23:23:58 +1100

    complete lab

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,11 @@
 // write your CatList component here
+function CatList({ catPics }){
+    const catImgs = catPics.map(catPic => <img alt="cat" src={catPic.url}/>)
+    return (
+        <div>
+            {catImgs}
+        </div>
+    )
+}
+
+export default CatList;
--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchCats } from "./catsSlice";
+import CatList from "./CatList";
 
 function Cats() {
+  const catPics = useSelector(state => state.entities);
+  const loadingStatus = useSelector(state => state.loading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCats());
+  }, []);
+
+
   return (
     <div>
       <h1>CatBook</h1>
       {/* add CatList component here */}
+      {loadingStatus === "loading" ? <h3>Loading...</h3> : <CatList catPics={catPics}/>}
     </div>
   );
 }
